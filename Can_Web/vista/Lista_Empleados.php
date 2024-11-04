da85bb6c8b6d9e62991fb42b334e55513567e294
gael773 <gael.diaz.guerrero2005@gmail.com>
2024-11-04 08:42:48 -0600

    Update Lista_Empleados.php

--- Can_Web/vista/Lista_Empleados.php
+++ Can_Web/vista/Lista_Empleados.php
@@ -1,7 +1,3 @@
-<?php 
-    $conexion=mysqli_connect('localhost','root','','lan_blast')
-    ?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -18,26 +14,26 @@
         <h1 id="t1">Gestion veterinaria</h1>
         <h1 id="t2">Clinica de mascotas MVPET</h1>
         <div class="clearfix"></div>
-        <nav>
+<nav>
         <a href="index.html">
                 <i class="fa-solid fa-right-to-bracket"></i> Salir</a>
-            <a href="lista_empleados.php">
+            <a href="lista_empleados.html">
                 <i class="fa-regular fa-person"></i> Empleado</a>
-            <a href="lista_clientes.php">
+            <a href="lista_clientes.html">
                 <i class="fa-solid fa-address-book"></i> Cliente</a>
-            <a href="lista_mascotas.php">
+            <a href="lista_mascotas.html">
                 <i class="fa-regular fa-dog"></i> Mascotas</a>
-            <a href="lista_Servicios.php">
+            <a href="lista_servicios.html">
                 <i class="fa-light fa-briefcase"></i> Servicios</a>
-            <a href="lista_usuarios.php">
+            <a href="lista_usuarios.html">
                 <i class="fa-solid fa-user"></i> Usuarios</a>
-            <a href="lista_productos.php">
+            <a href="lista_productos.html">
             <i class="fa-solid fa-shelves"></i> Inventario</a>
             <a href="generarcomprobantes.html">
             <i class="fa-thin fa-file-medical"></i> Comprobantes</a>
             <a href="cita.html">
                <i class="fa-solid fa-books-medical"></i> Citas</a>
-            <a href="Historial_clinico.html">
+            <a href="historial_clinico.html">
             <i class="fa-solid fa-notes-medical"></i>Historial Clinico</a>
                     
         </nav>
@@ -57,11 +53,7 @@
                 <th colspan="2">Operaciones</th>
 
             </tr>
-            <?php
-                $sql="SELECT * from empleado";
-                $result = mysqli_query($conexion,$sql);
-                while($mostrar=mysqli_fetch_array($result)){
-                    ?>
+            
             <tr>
                 <td><?php echo $mostrar['ID']?></td>
                 <td><?php echo $mostrar['Nombre']?></td>
